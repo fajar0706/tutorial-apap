a996e8217cb065e31be0b4ded36c73b715aa965e
fajar anugerah <jarwo9767@gmail.com>
2020-12-11 19:13:53 +0700

    feat/tutorial-8-frontend

---
+++ frontend-traveloke/src/components/Hotel/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "../Button";
+import Kamar from "../Kamar";
+import classes from "./styles.module.css";
+
+
+const Hotel = (props) => {
+    const {id, namaHotel, alamat, nomorTelepon,handleEdit, handleDelete, listKamar} = props;
+
+return (
+    <div className={classes.hotel}>
+        <h3>{`ID Hotel ${id}`}</h3>
+        <p>{`Nama Hotel: ${namaHotel}`}</p>
+        <p>{`Alamat: ${alamat}`}</p>
+        <p>{`Nomor telepon: ${nomorTelepon}`}</p>
+        <div>
+            <p className={classes.bold}>Kamar: </p>
+            {listKamar.length !==0 ?(
+                <div>
+                    {listKamar.map((kamar) => (
+                        <Kamar
+                        key={kamar.id} 
+                        namaKamar={kamar.namaKamar} 
+                        kapasitasKamar={kamar.kapasitasKamar} />
+                    ))}
+                </div>
+            ): <p className={classes.bold}>Hotel tidak memiliki Kamar</p>}
+        </div>
+        <Button onClick={handleEdit} variant="success">
+        Edit
+        </Button>
+        <Button onClick={handleDelete} variant="danger">
+        Delete
+        </Button>
+    </div>
+    );
+};
+
+export default Hotel;
--- traveloke/src/main/java/apap/tutorial/traveloke/security/WebSecurityConfig.java
+++ traveloke/src/main/java/apap/tutorial/traveloke/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -21,9 +22,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
+//                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and()
                 .authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/js/**").permitAll()
+                .antMatchers("/api/v1/**").permitAll()
                 .antMatchers("/kamar/add/**").hasAuthority("RECEPTIONIST")
                 .antMatchers("/user/addUser").hasAuthority("ADMIN")
                 .antMatchers("/user/updatePassword").hasAuthority("USER")
@@ -32,7 +35,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll()
+                .and()
+                .cors()
+                .and()
+                .csrf()
+                .disable();
     }
     @Bean
     public BCryptPasswordEncoder encoder(){ return new BCryptPasswordEncoder();}
